99a91bf9350d5e69cf1d5dc3d886763dc15ffa7d
Celeo <celeodor@gmail.com>
2016-01-20 13:13:30 -0800

    Switched from flask-redis to celery
    Switched from flask-redis to celery

--- tasks/app.py
+++ tasks/app.py
@@ -1,9 +1,7 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
-from flask.ext.redis import FlaskRedis
 from datetime import timedelta
-from threading import Thread
-import time
+from celery import Celery
 from tasks.shared import db, app_settings
 
 import eventlet
@@ -15,37 +13,35 @@ app.config.from_pyfile('config.cfg')
 for k, v in app.config.items():
     app_settings[k] = v
 app.permanent_session_lifetime = timedelta(days=14)
+
 db.app = app
 db.init_app(app)
+
 socketio = SocketIO(app, async_mode='eventlet')
-redis_store = FlaskRedis(app)
-thread = None
+
+celery = Celery(app.import_name, broker=app.config['CELERY_BROKER_URL'])
+celery.conf.update(app.config)
+TaskBase = celery.Task
+
+
+class ContextTask(TaskBase):
+    abstract = True
+
+    def __call__(self, *args, **kwargs):
+        with app.app_context():
+            return TaskBase.__call__(self, *args, **kwargs)
+
+celery.Task = ContextTask
 
 
 @app.route('/')
 def index():
-    global thread
-    if thread is None:
-        thread = Thread(target=background_task)
-        thread.daemon = True
-        thread.start()
     return render_template('index.html')
 
 
 @socketio.on('my event', namespace='/test')
 def test_message(message):
     emit('my response', {'data': 'Backend saw "' + message['data'] + '" from the frontend'})
-
-
-def background_task():
-    count = 0
-    while True:
-        time.sleep(1)
-        count += 1
-        print('Clock tick')
-        socketio.emit('my response',
-                      {'data': 'Server generated event', 'count': count},
-                      namespace='/test')
 
 
 if __name__ == '__main__':
